1f643381240df19bfee209cbe240220cb5e3c9df
Winnie-Irene <i.winnie@alustudent.com>
2026-05-28 12:14:22 +0200

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,35 +1,15 @@
 #!/usr/bin/env python3
 
-
-"""
-add
----
-
-This function takes two float numbers, `a` and `b`, and
-returns their sum as a float.
-
-Parameters:
-    a (float): The first float number.
-    b (float): The second float number.
-
-Returns:
-    float: The sum of `a` and `b`.
-
-Example:
-    >>> add(2.0, 3.0)
-    5.0
-"""
+"""This module contains a function that adds two float numbers together."""
 
 
 def add(a: float, b: float) -> float:
     """
-    Add two float numbers and return their sum.
-
-    Args:
-        a (float): The first float number.
-        b (float): The second float number.
-
+    Takes two float numbers a and b and returns their sum as a float.
+    Parameters:
+    a (float): The first number to be added.
+    b (float): The second number to be added.
     Returns:
-        float: The sum of `a` and `b`.
+    float: The sum of the two numbers.
     """
     return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,34 +1,12 @@
 #!/usr/bin/env python3
 
-
 """
-This module contains a function that
-concatenates two strings.
-The function takes two strings as arguments and
-returns a concatenated string
-
-Example:
-    >>> concat("Hello ", "World!")
-    'Hello World!'
-    >>> concat("Hello ", 42)
-    'Hello 42'
+Basic concat that takes a string str1 and a string str2 as arguments
 """
 
 
 def concat(str1: str, str2: str) -> str:
     """
-    Concatenate two strings and return the result.
-
-    Args:
-        str1 (str): The first string to concatenate.
-        str2 (str): The second string to concatenate.
-
-    Returns:
-        str: The concatenated string.
-
-    Example:
-        >>> concat("Hello ", "World!")
-        'Hello World!'
+    A function that returns a concatenated string
     """
-
     return str1 + str2
